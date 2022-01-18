9172111a45268afd55f92a6d4886e2f39933983f
Jack Phan <phan92@otachi.uwb.edu>
2022-01-18 10:20:04 -0800

    Multiple updates

---
+++ src/config/config.py
@@ -0,0 +1,14 @@
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+# Used in DataFilePlayGround_2.py
+# Path to raw data
+RAW_DATA_PATH = str(os.getenv("RAW_DATA_PATH"))
+# Path to processed numpy matrices
+PROCESSED_NUMPY_PATH = str(os.getenv("PROCESSED_NUMPY_PATH"))
+# Path to trained models
+TRAINED_MODELS_PATH = str(os.getenv("TRAINED_MODELS_PATH"))
+# Path to saved inference results
+INFERENCE_PATH = str(os.getenv("INFERENCE_PATH"))
