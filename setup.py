857961fe5b5ee6ad40cb3ecb8a24c4b22c3563a6
wysohn <wysohn2002@naver.com>
2022-05-29 12:13:21 -0700

    seperate requirements

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 from setuptools import setup
 
+# https://stackoverflow.com/questions/26900328/install-dependencies-from-setup-py
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='TBI Diagnosis',
     version='1.0',
     description='Diagnosis of TBI using Pulsatility Ultrasound',
     packages=['src'],
-    install_requires=[
-        'numpy',
-        'h5py',
-        'tensorflow',
-        'tensorboard',
-        'tensorflow-gpu',
-        'Jupyter',
-        'keras',
-        'matplotlib',
-        'opencv-python',
-        'scikit-learn',
-        'scipy'
-    ]
+    install_requires=install_requires
 )
